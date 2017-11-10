b15e276e9553a839b7639bbacf78234420a23f79
Joern Ott <joern.ott@ott-consult.de>
2017-11-10 12:43:52 +0000

    Add ssh-Install

--- src/tmp/install/oc-cloud9.sh
+++ src/tmp/install/oc-cloud9.sh
@@ -12,10 +12,16 @@ git clone https://github.com/c9/core.git /cloud9
 cd /cloud9
 ./scripts/install-sdk.sh
 sed -e 's/127\.0\.0\.1/0.0.0.0/' -i /cloud9/configs/standalone.js
-cleanup git make gcc cpp fipscheck fipscheck-lib glibc-devel glibc-headers groff-base kernel-headers less \
-        libedit libgnome-keyring libgomp libmpc mpfr openssh openssh-clients perl-Carp perl-Encode perl-Error \
-        perl-Exporter perl-File-Path perl-File-Temp perl-Filter perl-Getopt-Long perl-Git perl-HTTP-Tiny perl-PathTools \
-        perl-Pod-Escapes perl-Pod-Perldoc perl-Pod-Simple perl-Pod-Usage perl-Scalar-List-Utils perl-Socket \
-        perl-Storable perl-TermReadKey perl-Text-ParseWords perl-Time-HiRes perl-Time-Local perl-constant perl-libs \
-        perl-macros perl-parent perl-podlators perl-threads perl-threads-shared rsync perl
+
+curl -sSo /tmp/install/cloud9_ssh_install.sh https://raw.githubusercontent.com/c9/install/master/install.sh
+chmod a+x /tmp/install/cloud9_ssh_install.sh
+cd ${APP_HOME}
+gosu ${APP_USER}:${APP_GROUP} /tmp/install/cloud9_ssh_install.sh
+
+#cleanup git make gcc cpp fipscheck fipscheck-lib glibc-devel glibc-headers groff-base kernel-headers less \
+#        libedit libgnome-keyring libgomp libmpc mpfr openssh openssh-clients perl-Carp perl-Encode perl-Error \
+#        perl-Exporter perl-File-Path perl-File-Temp perl-Filter perl-Getopt-Long perl-Git perl-HTTP-Tiny perl-PathTools \
+#        perl-Pod-Escapes perl-Pod-Perldoc perl-Pod-Simple perl-Pod-Usage perl-Scalar-List-Utils perl-Socket \
+#        perl-Storable perl-TermReadKey perl-Text-ParseWords perl-Time-HiRes perl-Time-Local perl-constant perl-libs \
+#        perl-macros perl-parent perl-podlators perl-threads perl-threads-shared rsync perl
         
